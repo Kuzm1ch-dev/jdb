718a21467aa2782169516ea859c1bbb5d728bf8f
Kuzm1ch <63616890+Kuzm1ch-dev@users.noreply.github.com>
2024-09-12 20:38:08 +0300

    order by parsing / update filter parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use nom::branch::{alt, permutation};
-use nom::bytes::complete::{take_until, take_while};
-use nom::character::complete::{alpha1, digit1, space0};
-use nom::character::is_alphabetic;
+use nom::bytes::complete::{take_till, take_until, take_while};
+use nom::character::complete::{alpha1, space0};
+use nom::error::{Error, ErrorKind, ParseError};
 use nom::multi::{many0, separated_list0};
-use nom::sequence::terminated;
+use nom::sequence::tuple;
 use nom::{
     bytes::complete::{tag, tag_no_case},
     character::complete::{alphanumeric1, space1},
@@ -61,62 +61,25 @@ fn parse_select(input: &str) -> IResult<&str, Query> {
         },
     ))
 }
-
 fn parse_binary_op(input: &str) -> IResult<&str, BinaryOp> {
-    alt((
-        parse_binary_op_is_eq,
-        parse_binary_op_is_not_eq,
-        parse_binary_op_is_gt,
-        parse_binary_op_is_gteq,
-        parse_binary_op_is_lt,
-        parse_binary_op_is_lteq,
-        parse_binary_op_is_or,
-        parse_binary_op_is_and,
-    ))(input)
-}
-
-fn parse_binary_op_is_eq(input: &str) -> IResult<&str, BinaryOp> {
-    let (input, _) = tag("=")(input)?;
-    Ok((input, BinaryOp::Eq))
-}
-
-fn parse_binary_op_is_not_eq(input: &str) -> IResult<&str, BinaryOp> {
-    let (input, _) = tag("!=")(input)?;
-    Ok((input, BinaryOp::NotEq))
-}
-
-fn parse_binary_op_is_gt(input: &str) -> IResult<&str, BinaryOp> {
-    let (input, _) = tag(">")(input)?;
-    Ok((input, BinaryOp::Gt))
-}
-
-fn parse_binary_op_is_gteq(input: &str) -> IResult<&str, BinaryOp> {
-    let (input, _) = tag(">=")(input)?;
-    Ok((input, BinaryOp::GtEq))
-}
-
-fn parse_binary_op_is_lt(input: &str) -> IResult<&str, BinaryOp> {
-    let (input, _) = tag("<")(input)?;
-    Ok((input, BinaryOp::Lt))
-}
-
-fn parse_binary_op_is_lteq(input: &str) -> IResult<&str, BinaryOp> {
-    let (input, _) = tag("<=")(input)?;
-    Ok((input, BinaryOp::LtEq))
-}
-
-fn parse_binary_op_is_or(input: &str) -> IResult<&str, BinaryOp> {
-    let (input, _) = tag("or")(input)?;
-    Ok((input, BinaryOp::Or))
-}
-
-fn parse_binary_op_is_and(input: &str) -> IResult<&str, BinaryOp> {
-    let (input, _) = tag("and")(input)?;
-    Ok((input, BinaryOp::And))
+    match take_until(" ")(input)? {
+        (remainder, "=") => Ok((remainder, BinaryOp::Eq)),
+        (remainder, "!=") => Ok((remainder, BinaryOp::NotEq)),
+        (remainder, ">") => Ok((remainder, BinaryOp::Gt)),
+        (remainder, ">=") => Ok((remainder, BinaryOp::GtEq)),
+        (remainder, "<") => Ok((remainder, BinaryOp::Lt)),
+        (remainder, "<=") => Ok((remainder, BinaryOp::LtEq)),
+        (remainder, "|") => Ok((remainder, BinaryOp::Or)),
+        (remainder, "&") => Ok((remainder, BinaryOp::And)),
+        _ => {
+            let e: ErrorKind = ErrorKind::Tag;
+            return Err(nom::Err::Error(Error::from_error_kind(input, e)));
+        }
+    }
 }
 
 fn parse_ident_or_value(input: &str) -> IResult<&str, Expr> {
-    let (input, _ ) = space0(input)?;
+    let (input, _) = space0(input)?;
     alt((parse_function, parse_ident, parse_value))(input)
 }
 
@@ -131,7 +94,8 @@ fn parse_ident(input: &str) -> IResult<&str, Expr> {
 }
 
 fn parse_function(input: &str) -> IResult<&str, Expr> {
-    let (input, (function, args)) = permutation((alphanumeric1, parse_separated_list_in_parentheses))(input)?;
+    let (input, (function, args)) =
+        tuple((alphanumeric1, parse_separated_list_in_parentheses))(input)?;
     Ok((
         input,
         Expr::Function {
@@ -151,40 +115,50 @@ fn parse_value(input: &str) -> IResult<&str, Expr> {
 }
 
 fn parse_filter_expr(input: &str) -> IResult<&str, Option<Expr>> {
-    let (input, _) = space1(input)?;
-    let (input, _) = tag_no_case("filter")(input)?;
-    let (input, _) = space1(input)?;
-    let (input, left) = parse_ident_or_value(input)?;
-    let (input, _) = space1(input)?;
-    let (input, op) = parse_binary_op(input)?;
-    let (input, _) = space1(input)?;
-    let (input, right) = parse_ident_or_value(input)?;
+    let (input, (_, _, _, left, _, op, _, right)) = tuple((
+        space1,
+        tag_no_case("filter"),
+        space1,
+        parse_ident_or_value,
+        space1,
+        parse_binary_op,
+        space1,
+        parse_ident_or_value,
+    ))(input)?;
     let expr = Expr::BinaryOp {
         left: Box::new(left),
         op,
         right: Box::new(right),
     };
     match parse_filter_expr_next(Some(expr.clone()), input) {
-        Err(_) => return Ok((input, Some(expr))),
-        Ok((r, e)) => Ok((r, e)),
+        Err(_) => {
+            return Ok((input, Some(expr)));
+        }
+        Ok((remainder, expr)) => Ok((remainder, expr)),
     }
 }
 
 fn parse_filter_expr_next(left: Option<Expr>, input: &str) -> IResult<&str, Option<Expr>> {
     match left {
         Some(left) => {
-            let (input, _) = space1(input)?;
-            let (input, op) = parse_binary_op(input)?;
-            let (input, _) = space1(input)?;
-            let (input, right) = parse_ident_or_value(input)?;
+            let (input, op, right) =
+                match tuple((space1, parse_binary_op, space1, parse_ident_or_value))(input) {
+                    Ok((input, (_, op, _, right))) => (input, op, right),
+                    _ => {
+                        let e: ErrorKind = ErrorKind::Tag;
+                        return Err(nom::Err::Error(Error::from_error_kind(input, e)));
+                    }
+                };
             let expr = Expr::BinaryOp {
                 left: Box::new(left),
                 op,
                 right: Box::new(right),
             };
             match parse_filter_expr_next(Some(expr.clone()), input) {
-                Err(_) => return Ok((input, Some(expr))),
-                Ok((r, e)) => Ok((r, e)),
+                Err(_) => {
+                    return Ok((input, Some(expr)));
+                }
+                Ok((remainder, expr)) => Ok((remainder, expr)),
             }
         }
         None => panic!("Ошибка парсинга!"),
@@ -199,8 +173,40 @@ fn parse_group_by_expr(input: &str) -> IResult<&str, Option<Expr>> {
     Ok((input, Some(Expr::GroupBy { columns: group_by })))
 }
 
+fn parse_order_by_expr(input: &str) -> IResult<&str, Option<Expr>> {
+    let (input, _) = space1(input)?;
+    let (input, _) = tag_no_case("order by")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, orders) = parse_orders_list(input)?;
+    Ok((input, Some(Expr::OrderBy { orders })))
+}
+
+fn parse_asc(input: &str) -> IResult<&str, SortingOrder> {
+    let (input, _) = tag_no_case("asc")(input)?;
+    Ok((input, SortingOrder::ASC))
+}
+
+fn parse_desc(input: &str) -> IResult<&str, SortingOrder> {
+    let (input, _) = tag_no_case("desc")(input)?;
+    Ok((input, SortingOrder::DESC))
+}
+
+fn parse_order(input: &str) -> IResult<&str, Order> {
+    let (input, (_, ident, _, order)) =
+        tuple((space0, parse_ident, space1, alt((parse_asc, parse_desc))))(input)?;
+    Ok((input, Order { ident, order }))
+}
+
+fn parse_orders_list(input: &str) -> IResult<&str, Vec<Order>> {
+    separated_list0(tag(","), parse_order)(input)
+}
+
 fn parse_exprs(input: &str) -> IResult<&str, Vec<Option<Expr>>> {
-    many0(alt((parse_filter_expr, parse_group_by_expr)))(input)
+    many0(alt((
+        parse_filter_expr,
+        parse_group_by_expr,
+        parse_order_by_expr,
+    )))(input)
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -232,8 +238,19 @@ pub enum BinaryOp {
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Identifier {
-    /// The value of the identifier without quotes.
     pub value: String,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Order {
+    pub ident: Expr,
+    pub order: SortingOrder,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum SortingOrder {
+    ASC,
+    DESC,
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -253,6 +270,9 @@ pub enum Expr {
     GroupBy {
         columns: Vec<Expr>,
     },
+    OrderBy {
+        orders: Vec<Order>,
+    },
     Subquery(Box<Query>),
 }
 #[derive(Debug, PartialEq, Clone)]
@@ -265,7 +285,7 @@ pub struct Query {
 // rem:  and co2 > 400  and co3 < 500, expr: {col1, >, 100}
 fn main() {
     let (r, o) = parse_select(
-        "select '15',function1(name),id,name,title from table filter id > 15 and name = 'sex' group by title",
+        "select '15',function1(name),id,name,title from table filter id > 100 & id < 200 group by id,name order by id asc, sex desc",
     )
     .unwrap();
     println!("{:?}", r);
